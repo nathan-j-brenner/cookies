607727090d10229ee1b9dc87e68b50c689943b5b
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-03 15:53:48 -0700

    cookie-parser works

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var cookieParser = require('cookie-parser');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	if (req.cookies.flavor){
+		console.log('yum,' + req.cookies.flavor);
+	} else{
+		res.cookie('flavor', 'banana');
+	}
+	res.render('index', { title: 'Twitter clone' });
 });
 
 module.exports = router;
